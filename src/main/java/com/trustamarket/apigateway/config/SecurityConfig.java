3ba13e38873cffc82bcfab9f94e913188fae76f9
ansdl <ansdlsgur0123@gmail.com>
2026-04-29 16:10:45 +0900

    [feature]  Gateway JWT 검증 및 SecurityConfig 구현

---
+++ src/main/java/com/trustamarket/apigateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.trustamarket.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/**").permitAll() // 비인가 링크추가하시면됩니다.
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
